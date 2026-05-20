f9b061abbeebf21813dbdeb58a933959d199261b
Jacopo Cafarelli <jacopo.cafarelli@gmail.com>
2026-05-20 08:47:35 +0200

    Setup Hilt for dependency injection

---
+++ app/src/main/java/com/jc/topstackoverflowusers/TopStackoverflowUsersApplication.kt
@@ -0,0 +1,7 @@
+package com.jc.topstackoverflowusers
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TopStackoverflowUsersApplication : Application()
